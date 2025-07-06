6c296dcf469e207e09f8048f6c9429aa88a87fbd
Willow <107652695+Willow-otst@users.noreply.github.com>
2025-07-06 12:56:29 -0500

    added menus file & edit

--- include/editorWindow.h
+++ include/editorWindow.h
@@ -27,7 +27,27 @@ public:
 enum {
     // menu items
 
+    // File
+    BAR_FILE_SAVE,
+    BAR_FILE_LOAD,
+    BAR_FILE_NEW,
+    BAR_FILE_RENAME,
+    BAR_FILE_HISTORY,
+    BAR_FILE_DETAILS,
+    BAR_FILE_DELETE_FILE,
+
+    // Edit
+    BAR_EDIT_UNDO,
+    BAR_EDIT_REDO,
+    BAR_EDIT_CUT,
+    BAR_EDIT_COPY,
+    BAR_EDIT_PASTE,
+    BAR_EDIT_SELECT_ALL,
+    BAR_EDIT_DELETE,
+    BAR_EDIT_FIND,
+    BAR_EDIT_REPLACE,
+
     //wx standard operations
-    CONTROL_STANDARD_QUIT = wxID_EXIT,
-    CONTROL_STANDARD_ABOUT = wxID_ABOUT
+    BAR_STANDARD_QUIT = wxID_EXIT,
+    BAR_STANDARD_ABOUT = wxID_ABOUT
 };
--- src/editorWindow.cpp
+++ src/editorWindow.cpp
@@ -9,7 +9,41 @@ bool GApp::OnInit() {
 }
 // Constructor
 EditorWindow::EditorWindow() : wxFrame(nullptr, wxID_ANY, "Grimoire") {
+    wxMenuBar *menuBar = new wxMenuBar;
 
+    // File
+    wxMenu *menuFile = new wxMenu();
+        menuFile->Append(BAR_FILE_SAVE,         "Save", "Save file to Database.");
+        menuFile->Append(BAR_FILE_LOAD,         "Load", "");
+        menuFile->Append(BAR_FILE_NEW,          "New", "");
+        menuFile->AppendSeparator();
+        menuFile->Append(BAR_FILE_RENAME,       "Rename", "");
+        menuFile->Append(BAR_FILE_HISTORY,      "History", "");
+        menuFile->AppendSeparator();
+        menuFile->Append(BAR_FILE_DETAILS,      "Details", "");
+        menuFile->Append(BAR_FILE_DELETE_FILE,  "Delete File", "");
+    menuBar->Append(menuFile, "&File");
+
+    wxMenu *menuEdit = new wxMenu();
+        menuEdit->Append(BAR_EDIT_UNDO,          "Undo", "");
+        menuEdit->Append(BAR_EDIT_REDO,          "Redo", "");
+        menuEdit->AppendSeparator();
+        menuEdit->Append(BAR_EDIT_CUT,           "Cut", "");
+        menuEdit->Append(BAR_EDIT_COPY,          "Copy", "");
+        menuEdit->Append(BAR_EDIT_PASTE,         "Paste", "");
+        menuEdit->AppendSeparator();
+        menuEdit->Append(BAR_EDIT_SELECT_ALL,    "Select All", "");
+        menuEdit->Append(BAR_EDIT_DELETE,        "Delete", "");
+        menuEdit->AppendSeparator();
+        menuEdit->Append(BAR_EDIT_FIND,          "Find", "");
+        menuEdit->Append(BAR_EDIT_REPLACE,       "Replace", "");
+    menuBar->Append(menuEdit, "&Edit");
+
+
+
+
+
+    SetMenuBar(menuBar);
 }
 // Deconstructor
 EditorWindow::~EditorWindow() {
