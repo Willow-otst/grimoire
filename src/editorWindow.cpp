7a24dd71458a6fb66124bf38e73ba1ab9920f154
Willow <107652695+Willow-otst@users.noreply.github.com>
2025-07-06 13:45:23 -0500

    added menus view, format & search

--- src/editorWindow.cpp
+++ src/editorWindow.cpp
@@ -13,34 +13,63 @@ EditorWindow::EditorWindow() : wxFrame(nullptr, wxID_ANY, "Grimoire") {
 
     // File
     wxMenu *menuFile = new wxMenu();
-        menuFile->Append(BAR_FILE_SAVE,         "Save", "Save file to Database.");
-        menuFile->Append(BAR_FILE_LOAD,         "Load", "");
-        menuFile->Append(BAR_FILE_NEW,          "New", "");
+        menuFile->Append(FILE_SAVE,         "Save", "Save file to Database.");
+        menuFile->Append(FILE_LOAD,         "Load", "");
+        menuFile->Append(FILE_NEW,          "New", "");
         menuFile->AppendSeparator();
-        menuFile->Append(BAR_FILE_RENAME,       "Rename", "");
-        menuFile->Append(BAR_FILE_HISTORY,      "History", "");
+        menuFile->Append(FILE_RENAME,       "Rename", "");
+        menuFile->Append(FILE_HISTORY,      "History", "");
         menuFile->AppendSeparator();
-        menuFile->Append(BAR_FILE_DETAILS,      "Details", "");
-        menuFile->Append(BAR_FILE_DELETE_FILE,  "Delete File", "");
+        menuFile->Append(FILE_DETAILS,      "Details", "");
+        menuFile->Append(FILE_DELETE_FILE,  "Delete File", "");
     menuBar->Append(menuFile, "&File");
 
     wxMenu *menuEdit = new wxMenu();
-        menuEdit->Append(BAR_EDIT_UNDO,          "Undo", "");
-        menuEdit->Append(BAR_EDIT_REDO,          "Redo", "");
+        menuEdit->Append(EDIT_UNDO,         "Undo", "");
+        menuEdit->Append(EDIT_REDO,         "Redo", "");
         menuEdit->AppendSeparator();
-        menuEdit->Append(BAR_EDIT_CUT,           "Cut", "");
-        menuEdit->Append(BAR_EDIT_COPY,          "Copy", "");
-        menuEdit->Append(BAR_EDIT_PASTE,         "Paste", "");
+        menuEdit->Append(EDIT_CUT,          "Cut", "");
+        menuEdit->Append(EDIT_COPY,         "Copy", "");
+        menuEdit->Append(EDIT_PASTE,        "Paste", "");
         menuEdit->AppendSeparator();
-        menuEdit->Append(BAR_EDIT_SELECT_ALL,    "Select All", "");
-        menuEdit->Append(BAR_EDIT_DELETE,        "Delete", "");
+        menuEdit->Append(EDIT_SELECT_ALL,   "Select All", "");
+        menuEdit->Append(EDIT_DELETE,       "Delete", "");
         menuEdit->AppendSeparator();
-        menuEdit->Append(BAR_EDIT_FIND,          "Find", "");
-        menuEdit->Append(BAR_EDIT_REPLACE,       "Replace", "");
+        menuEdit->Append(EDIT_FIND,         "Find", "");
+        menuEdit->Append(EDIT_REPLACE,      "Replace", "");
     menuBar->Append(menuEdit, "&Edit");
 
+    wxMenu *menuView = new wxMenu();
+        menuView->Append(VIEW_ZOOM_IN,      "Zoom In", "");
+        menuView->Append(VIEW_ZOOM_OUT,     "Zoom Out", "");
+    menuBar->Append(menuView, "&View");
 
+    wxMenu *menuFormat = new wxMenu();
+        wxMenu *menuText = new wxMenu();
+            menuText->Append(FORMAT_TEXT_BOLD,          "Bold", "");
+            menuText->Append(FORMAT_TEXT_ITALIC,        "Italic", "");
+            menuText->Append(FORMAT_TEXT_UNDERLINE,     "Underline", "");
+            menuText->Append(FORMAT_TEXT_STIKETHROUGH,  "Strikethrough", "");
+        menuFormat->AppendSubMenu(menuText, "Text");
 
+        wxMenu *menuList = new wxMenu();
+            menuList->Append(FORMAT_LIST_BULLET,        "Bullet", "");
+            menuList->Append(FORMAT_LIST_NUMBER,        "Number", "");
+            menuList->Append(FORMAT_LIST_PLAIN,         "Plain", "");
+        menuFormat->AppendSubMenu(menuList, "List");
+
+        wxMenu *menuIndent = new wxMenu();
+            menuIndent->Append(FORMAT_INDENT_INCREASE,  "Increase", "");
+            menuIndent->Append(FORMAT_INDENT_DECREASE,  "Decrease", "");
+        menuFormat->AppendSubMenu(menuIndent, "Indent");
+    menuBar->Append(menuFormat, "For&mat");
+
+    wxMenu *menuSearch = new wxMenu();
+        menuSearch->Append(SEARCH_FILE,             "Search File", "");
+        menuSearch->Append(SEARCH_FILE_ARCHIVE,     "Search File Archive", "");
+        menuSearch->Append(SEARCH_GRIMOIRE,         "Search Grimoire", "");
+        menuSearch->Append(SEARCH_GRIMOIRE_ARCHIVE, "Search Grimoire Archive", "");
+    menuBar->Append(menuSearch, "&Search");
 
 
     SetMenuBar(menuBar);
